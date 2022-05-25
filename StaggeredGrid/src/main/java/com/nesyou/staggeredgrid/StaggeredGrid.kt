d53873a6df6b476c0dd2c9991154e7c8225c6f18
Younes Lagmah <uneslagmah@gmail.com>
2022-05-25 15:12:34 +0100

    replace nested scroll with scrollable

--- StaggeredGrid/src/main/java/com/nesyou/staggeredgrid/StaggeredGrid.kt
+++ StaggeredGrid/src/main/java/com/nesyou/staggeredgrid/StaggeredGrid.kt
@@ -1,11 +1,7 @@
 package com.nesyou.staggeredgrid
 
 
-import androidx.compose.animation.core.AnimationState
-import androidx.compose.animation.core.animateDecay
-import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -13,16 +9,10 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.*
-import kotlin.math.abs
 
 /**
  * The composable of staggered grid
@@ -75,86 +65,38 @@ internal fun StaggeredGrid(
     }
     val layoutDirection = LocalLayoutDirection.current
     val coroutineScope = rememberCoroutineScope()
-    val flingDecay = rememberSplineBasedDecay<Float>()
-    suspend fun flingBehavior(
-        i: Float,
-        state: ScrollableState,
-    ) {
-        if (abs(i) > 1f) {
-            var velocityLeft = i
-            var lastValue = 0f
-            AnimationState(
-                initialValue = 0f,
-                initialVelocity = i,
-            ).animateDecay(flingDecay) {
-                val delta = value - lastValue
-                lastValue = value
-                velocityLeft = this.velocity
-                val c = state.dispatchRawDelta(delta)
-                if (abs(delta - c) > 0.5f) this.cancelAnimation()
-            }
-            state.scrollBy(velocityLeft)
-        } else {
-            state.scrollBy(i)
-        }
+
+    val scroll = rememberScrollableState { delta ->
+        coroutineScope.launch { states.forEach { it.scrollBy(-delta) } }
+        delta
     }
 
-    val nestedScroll = object : NestedScrollConnection {
-        var job: MutableList<Job> = mutableListOf()
-        override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
-            job.forEach { it.cancel() }
-            job.clear()
-            job = coroutineScope {
-                states.map {
-                    launch {
-                        flingBehavior(-available.y, it)
-                    }
-                }
-            } as MutableList<Job>
-            return super.onPostFling(consumed, available)
-        }
-
-        override fun onPostScroll(
-            consumed: Offset,
-            available: Offset,
-            source: NestedScrollSource
-        ): Offset {
-            job.forEach { it.cancel() }
-            job.clear()
-            job.add(
-                coroutineScope.launch {
-                    states.forEach {
-                        it.scrollBy(-available.y)
-                    }
-                },
-            )
-            return super.onPostScroll(consumed, available, source)
-        }
-    }
-    Box(
-        Modifier.nestedScroll(nestedScroll)
+    Row(
+        modifier = Modifier.scrollable(
+            scroll,
+            Orientation.Vertical,
+            flingBehavior = ScrollableDefaults.flingBehavior()
+        )
     ) {
-        Row {
-            repeat(columnsNumber) {
-                LazyColumn(
-                    modifier = Modifier
-                        .weight(1F),
-                    state = states[it],
-                    contentPadding = PaddingValues(
-                        start = if (it == 0) padding.calculateLeftPadding(layoutDirection) else 0.dp,
-                        end = if (it == columnsNumber - 1) padding.calculateRightPadding(
-                            layoutDirection
-                        ) else 0.dp,
-                        top = padding.calculateTopPadding(),
-                        bottom = padding.calculateBottomPadding()
-                    ),
-                    userScrollEnabled = false
-                ) {
-                    for (i in scope.content.indices step columnsNumber) {
-                        if (scope.content.size > i + it) {
-                            item {
-                                scope.content[i + it]()
-                            }
+        repeat(columnsNumber) {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1F),
+                state = states[it],
+                contentPadding = PaddingValues(
+                    start = if (it == 0) padding.calculateLeftPadding(layoutDirection) else 0.dp,
+                    end = if (it == columnsNumber - 1) padding.calculateRightPadding(
+                        layoutDirection
+                    ) else 0.dp,
+                    top = padding.calculateTopPadding(),
+                    bottom = padding.calculateBottomPadding()
+                ),
+                userScrollEnabled = false
+            ) {
+                for (i in scope.content.indices step columnsNumber) {
+                    if (scope.content.size > i + it) {
+                        item {
+                            scope.content[i + it]()
                         }
                     }
                 }
